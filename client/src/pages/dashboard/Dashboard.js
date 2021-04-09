ae4c02c3f699d4c1dbae98fdf111ffd32c1b6dfb
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-04-09 08:39:30 -0700

    import semantic ui at index.js

---
+++ client/src/pages/dashboard/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+const Dashboard = () => {
+    return (
+        <h1>Hello World</h1>
+    )
+}
+
+export default Dashboard;
