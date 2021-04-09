5852b793ab007e2baac43e1448f8e2de13bd5bf2
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-04-09 09:32:32 -0700

    add dashboard page

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -1,9 +1,46 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Grid } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
     return (
-        <h1>Hello World</h1>
+        <div>
+            <div>
+                <Link to='/'><h1>Home</h1></Link>
+            </div>
+            <Grid doubling columns={5} style={{ height: '100vh'}}>
+                <Grid.Column>
+                    <div>
+                        <Link to="/game">Play!</Link>
+                    </div>
+                </Grid.Column>
+                <Grid.Column>
+                    <div>
+                        <Link to="/challenge">Challenge!</Link>
+                    </div>
+                </Grid.Column>
+                <Grid.Column>
+                    <div>
+                        <Link to="/flashcards">Flash Cards!</Link>
+                    </div>
+                </Grid.Column>
+                <Grid.Column>
+                    <div>
+                        <Link to="/stats">Stats!</Link>
+                    </div>
+                </Grid.Column>
+                <Grid.Column>
+                    <div>
+                        <Link to="/settings">Settings!</Link>
+                    </div>
+                </Grid.Column>
+                <Grid.Column>
+                    <div>
+                        <Link to="/teambio">Team</Link>
+                    </div>
+                </Grid.Column>
+            </Grid>
+        </div>
     )
 }
 
