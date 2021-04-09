fa5b0fb808c230cb3d044238e5ceacddb0bef354
rasputinforever <erik.justin.portillo@gmail.com>
2021-04-09 12:06:52 -0700

    dummy db added

---
+++ client/src/components/Challenge/Challenges.js
@@ -0,0 +1,25 @@
+// this is a multiple choice app, user gets 4 images and user selects the image that matches the word
+// user picks a "category": Farm Animals, Zoo, Fruits, Vegetables, Geography (mountains, rivers, etc)
+// user sees 4 images and a word
+    // can the word have an audio?
+// pick ANY and all words are shown
+// pick CORRECT and the user is rewarded with a happy animation
+// adds to score for user to DB, sends up the score
+// goe to next
+// do this 5 times
+// exit at will
+
+// data
+// random words from category API
+// small images from API
+// MAKE SURE THESE ARE KID FRIENDLY
+// Mongoose??? What are we storing here?
+// Stats => Users Schema
+
+// functionality
+    // get 4 words from API
+    // get associated pictures from API
+    // randomly pick ONE word to be the the user word
+    // RENDER 4 images and 1 word
+    // onclick: if word=image ? Success : No Points
+    // gotonext
