775f1e4958f3acc378989f2250ab042e292f1449
kurtp23 <kurt_phillips@live.com>
2021-04-09 19:01:32 -0700

    exported firebase initialize into util

---
+++ client/src/utils/fireUtil.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+export default firebase.initializeApp({
+  apiKey: "AIzaSyB4QCoJnYPe0SO57vaRjY2n2RjkzFJIjzo",
+  authDomain: "language-app-9fdd7.firebaseapp.com",
+  projectId: "language-app-9fdd7",
+  storageBucket: "language-app-9fdd7.appspot.com",
+  messagingSenderId: "322107717238",
+  appId: "1:322107717238:web:6ff906f60e98e96f349157",
+  measurementId: "G-4X4FT4CYN6",
+});
