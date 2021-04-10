f49b574505d04af14cdedfc4208825d2ea1fdd2e
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-04-10 13:46:29 -0700

    add state to flashcard component

--- client/src/pages/flash-cards/FlashCard.js
+++ client/src/pages/flash-cards/FlashCard.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import FlashCardTemplate from '../../components/flash-card-template/FlashCardTemplate';
+import API from '../../utils/API';
 
 const FlashCard = () => {
+
+    const [flashCardList, setFlashCardList] = useState([])
+
+    useEffect(() => {
+        API.getFlashcards()
+        .then((data) => {
+            let fcList = []
+            data.data.map(item => {
+                fcList.push({                  
+                    word: item.word, 
+                    englishWord: item.englishWord
+                }) 
+            })
+            console.log('fclist is', fcList)
+            setFlashCardList(fcList);
+
+            })
+
+    }, [])
+    console.log('flashcardlist is', flashCardList);
     return (
         <div>
             <h1>This is the flash card page</h1>
