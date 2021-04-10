23e62539b982344e137a50b98f5e45c94acd67b9
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-04-10 12:05:13 -0700

    fix bad commit - finish flashCardApi route

--- routes/flashCardApi.js
+++ routes/flashCardApi.js
@@ -1,6 +1,5 @@
 const Router = require("express").Router();
 const { Flashcard } = require("../models");
-const User = require("../models/User");
 
 Router.post("/api/flashCard", async (req, res) => {
     const testFlashCard = new Flashcard(body);
