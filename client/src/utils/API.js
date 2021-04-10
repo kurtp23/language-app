d87cc52d084396519ba9e58554fa1fefae36afad
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-04-10 13:06:30 -0700

    add getFlashcards method to API

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,5 +4,9 @@ export default {
   // Gets all books
   getChallenges: function() {
     return axios.get("/api/challenges");
-  }
+  },
+  // Gets all flashcards
+  getFlashcards: function() {
+    return axios.get("/api/flashcards");
+  },
 };
