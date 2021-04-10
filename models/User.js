cc18b1cb82dd4de920a4ab0c54d6c45382833e9a
kurtp23 <kurt_phillips@live.com>
2021-04-10 09:33:06 -0700

    updated user model

--- models/User.js
+++ models/User.js
@@ -2,9 +2,28 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    user: { type: String, required: "Must have a username!" },
+  user: { type: String, required: "Must have a username!" },
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    trim: true,
+    required: "Username is Required",
+  },
+
+  email: {
+    type: String,
+    unique: true,
+    match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+  },
+
+  userCreated: {
+    type: Date,
+    default: Date.now,
+  },
+});
