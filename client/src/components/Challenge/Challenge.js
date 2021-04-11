88db841de463ffad5f6426678a4e6d3cda8c870e
rasputinforever <erik.justin.portillo@gmail.com>
2021-04-11 11:27:02 -0700

    pseudocode in todo list

--- client/src/components/Challenge/Challenge.js
+++ client/src/components/Challenge/Challenge.js
@@ -7,23 +7,12 @@ import Choice from './Choice.js'
 import API from '../../utils/API.js'
 
 function Challenge({ category }){
-    // save category specific choices here, we want both the THING and a "hasBeenSelected" key so there are no repeats.
-    // will render FOUR things at a time as Cards
-    // need to call an API for the translations, they will be hidden
-    
-    // ONE of the four will RANDOMLY be assigned "Answer".
-    // the "answer" value will be rendered as a visible card, the user has to click on the card they think it matches
-        // add any audio cues here too, like "click to hear the word"
-    // When CARD is selected, 
-        // WRONG: translation is revealed, english and spanish. Can no longer select that card
-        // CORRECT: a "happy" message is revealed, the rest of the cards are also revealed
-            // this specific value is marked as "hasBeenSelected"
-        // All cards will have the audio available to listen to if they want
-        // User given option to "goto Next" or "exit"
-            // goto Next => re-render the next four, do NOT re-render a "hasBeenSelected"
-                // once the user hits data.length = 4 the game forces an exit
-
-            // onExit => send up the number of rounds to the DB with the DATE of play, keep it simple for now. Add more data later
+    // TODO LIST:
+    // Add translations into the mix
+    // Add Images
+    // Add "Exit Game" Button
+    // STYLING
+    // modulation!
     const [data, setData] = React.useState([])
     const [nextData, setNextData] = React.useState([])
 
