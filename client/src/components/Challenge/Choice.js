e7a88c1967cadab48e82dad93136c64226f3508d
rasputinforever <erik.justin.portillo@gmail.com>
2021-04-11 09:39:27 -0700

    conditional styling for correct answer

--- client/src/components/Challenge/Choice.js
+++ client/src/components/Challenge/Choice.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import { Card, Button, Image } from 'semantic-ui-react'
 
+import './Challenge.css'
+
 function Choice ({ value, correct, onChange }) {
     const [picked, setPicked] = React.useState(false)
     const imgString = value + ".PNG"
 
     function onSelection(e) {
-        console.log(correct)
         setPicked(true)
         onChange(e)
     }
 
-    const revealedAnswer = <Card.Header>{value}</Card.Header>
+    const revealedAnswer = <Card.Header style={correct ? { color: 'green'} : { color: 'red'}}>{value}</Card.Header>
 
     return(
         <Card>
         <Image src={imgString} wrapped ui={false} />
         <Card.Content>
-            {!picked ? <></> : <Card.Header>{value}</Card.Header>}
+            {!picked ? <></> : revealedAnswer}
           <Button value={value} onClick={onSelection}>Pick Me!</Button>
         </Card.Content>
       </Card>
