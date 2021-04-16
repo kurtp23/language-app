d614785f2f617f50e86c156641cbb16edfc5f181
rasputinforever <erik.justin.portillo@gmail.com>
2021-04-15 18:29:51 -0700

    button animated

--- client/src/components/Challenge/Category.js
+++ client/src/components/Challenge/Category.js
@@ -3,11 +3,11 @@ import { useSpring, animated } from 'react-spring'
 
 import { Card, Button, Image } from 'semantic-ui-react'
  
-const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
+const calc = (x, y) => [0, 1, 1.1]
 const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
 
 function Category({ category, onChange, description }) {
-    const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
+    const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 500, friction: 40 } }))
 
     function handleClick(e){
         onChange(e.target.value)
@@ -17,14 +17,9 @@ function Category({ category, onChange, description }) {
 
     return (
         <>
-            <animated.div
-            class="card"
-            onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
-            onMouseLeave={() => set({ xys: [0, 0, 1] })}
-            style={{ transform: props.xys.interpolate(trans) }}
-            >
- <div className="ui raised very padded text container segment">
-        <Card>
+            
+
+             <Card>
                 <Card.Content>
                     <Image
                     floated='right'
@@ -37,17 +32,25 @@ function Category({ category, onChange, description }) {
                     </Card.Description>
                 </Card.Content>
                 <Card.Content extra>
+                    <animated.div
+                        class="card"
+                        onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+                        onMouseLeave={() => set({ xys: [0, 0, 1] })}
+                        style={{ transform: props.xys.interpolate(trans) }}
+                        >
                     <div className='ui two buttons'>
                     <Button value={category} basic color='green' onClick={handleClick}>
                         Select
                     </Button>
                     </div>
-                </Card.Content>
-            </Card>
-            </div>
 
-    </animated.div>
-       
+                    </animated.div>
+
+                </Card.Content>
+
+            </Card>
+
+
         </>
     )
 }
