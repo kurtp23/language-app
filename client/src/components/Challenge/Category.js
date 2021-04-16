c68b4c98b5703ea4f8e31008c95b1bf292d307ed
rasputinforever <erik.justin.portillo@gmail.com>
2021-04-15 18:19:25 -0700

    spring added to categories

--- client/src/components/Challenge/Category.js
+++ client/src/components/Challenge/Category.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useSpring, animated } from 'react-spring'
 
 import { Card, Button, Image } from 'semantic-ui-react'
  
+const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
+const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+
 function Category({ category, onChange, description }) {
+    const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
 
     function handleClick(e){
         onChange(e.target.value)
@@ -12,8 +17,14 @@ function Category({ category, onChange, description }) {
 
     return (
         <>
-        <div className="ui raised very padded text container segment">
-            <Card>
+            <animated.div
+            class="card"
+            onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+            onMouseLeave={() => set({ xys: [0, 0, 1] })}
+            style={{ transform: props.xys.interpolate(trans) }}
+            >
+ <div className="ui raised very padded text container segment">
+        <Card>
                 <Card.Content>
                     <Image
                     floated='right'
@@ -34,6 +45,9 @@ function Category({ category, onChange, description }) {
                 </Card.Content>
             </Card>
             </div>
+
+    </animated.div>
+       
         </>
     )
 }
