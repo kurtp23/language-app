32bee39f308c7552cdc555b3cbe6771f3bc2274d
rasputinforever <erik.justin.portillo@gmail.com>
2021-04-15 19:08:08 -0700

    cleanup

--- client/src/components/Challenge/Choice.js
+++ client/src/components/Challenge/Choice.js
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { Card, Button, Image } from 'semantic-ui-react'
 import { useSpring, animated } from 'react-spring'
 
 function Choice ({ name, value, correct, onChange }) {
-    const [picked, setPicked] = React.useState(false)
     const imgString = "/images/Challenge/Choices/" + name.replace(/\s/g, "") + ".PNG"
 
     const [flipped, set] = useState(false)
@@ -12,29 +11,20 @@ function Choice ({ name, value, correct, onChange }) {
       transform: `perspective(300px) rotateX(${flipped ? 360 : 0}deg)`,
       config: { mass: 5, tension: 600, friction: 40 }
     })
-    
-    useEffect(() => {
-        setPicked(false)
-    }, [])
-
 
     function onSelection(e) {
-      
 
-        const picked = e.target.value
+        const answerPicked = e.target.value
         set(state => !state)
 
-        setTimeout(function(){
-            
-          onChange(picked)
-       }, 1000);
-
-
+        setTimeout(function(){      
+          onChange(answerPicked)
+       }, 2000);
     }
 
     const revealedAnswer = <Card.Header style={correct ? { color: 'green'} : { color: 'red'}}>{value}</Card.Header>
 
-    return(
+    return (
         <Card>
         <Image src={imgString} wrapped ui={false} />
         <Card.Content>
@@ -48,9 +38,6 @@ function Choice ({ name, value, correct, onChange }) {
           </animated.div>
         </Card.Content>
       </Card>
-
-
-
     )
 }
 
