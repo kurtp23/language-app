14f0639c14f8a42eca84e758cb557802896761bd
Destiny Fleming <destiny.fleming7401@gmail.com>
2021-04-17 13:48:08 -0700

    connecting user id to stats db

--- models/stats.js
+++ models/stats.js
@@ -1,20 +1,21 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const statsSchema = new Schema({
-    date: {
-      type: Date,
-      default: Date.now
-    },
-    flashcardVal: {
-      type: Number,
-      required: "enter flashcard amount"
-    },
-    challengeVal: {
-      type: Number,
-      required: "enter challenges amount"
-    }    
+const infoSchema = new Schema({
+  date: { type: Date, default: Date.now },
+  flashcardVal: { type: Number, default:0 },
+  challengeVal: { type: Number, default:0 }
+})
+
+const statsSchema = new Schema({ 
+    userID:  {
+      type: String,
+      required: true
+    }, 
+    stats:[infoSchema]
   });
+
+
 
 const Stats = mongoose.model('Stats', statsSchema);
 
