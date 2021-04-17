913c7b07039943424df41bee60e5d5350a23566d
e4k9 <eh26steve@gmail.com>
2021-04-17 10:14:49 -0700

    prototype game

---
+++ client/src/components/CardBoard/Card.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Card.css';
+
+
+
+class Card extends React.Component {
+  render() {
+    let content;
+    if(this.props.faceUp) {
+      content = this.props.content;
+    } else {
+      content = ''
+    }
+    return (
+      <div onClick={this.props.flip} className={`Card ${this.props.faceUp ? 'face-up': ''}`}>
+        {content}
+      </div>
+    )
+  }
+}
+
+export default Card;
