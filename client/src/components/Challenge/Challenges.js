fa1a6c71ebb95a81e5d836104c889c192c9cd403
rasputinforever <erik.justin.portillo@gmail.com>
2021-04-16 20:58:15 -0700

    more styling

--- client/src/components/Challenge/Challenges.js
+++ client/src/components/Challenge/Challenges.js
@@ -1,29 +1,4 @@
-// this is a multiple choice app, user gets 4 images and user selects the image that matches the word
-// user picks a "category": Farm Animals, Zoo, Fruits, Vegetables, Geography (mountains, rivers, etc)
-// user sees 4 images and a word
-    // can the word have an audio?
-// pick ANY and all words are shown
-// pick CORRECT and the user is rewarded with a happy animation
-// adds to score for user to DB, sends up the score
-// goe to next
-// do this 5 times
-// exit at will
-
-// data
-// random words from category API
-// small images from API: https://pixabay.com/api/?key=19527879-11b24e10aa7a260d8c2bad18b&q=${term-here}&image_type=photo
-    // this returns an array of images. Look at "tags" to get an exact match, otherwise you get weird other things. Tags = "Farm Animal"
-// MAKE SURE THESE ARE KID FRIENDLY
-// Mongoose??? What are we storing here? Set wordlist <- prevents any uncertainty with what is displayed
-// Stats => Users Schema
-
-// functionality
-    // get 4 words from API
-    // get associated pictures from API
-    // randomly pick ONE word to be the the user word
-    // RENDER 4 images and 1 word
-    // onclick: if word=image ? Success : No Points
-    // gotonext
+// TODO: Send to STATS via sats API
 
 import React, { useEffect } from 'react';
 
@@ -33,7 +8,7 @@ import Challenge from './Challenge.js'
 
 // API calls
 import API from '../../utils/API.js'
-import { Card } from 'semantic-ui-react';
+import { Card, Header, Container, Icon, Divider } from 'semantic-ui-react';
 
 // semantic ui
 
@@ -69,11 +44,15 @@ function Challenges() {
 
     return (
         <>
-        <div className="ui raised very padded text container segment">
+        <div className="ui raised text container segment" >
             
-            {!selection ? <h2 className="ui orange header">Select a Category!</h2> : <></>}
+            {!selection ? 
+            <Container textAlign='center'><Header className="ui orange header" as='h2'><Icon name='book' />Choose a Category</Header></Container> : 
+            <Container textAlign='center'><Header className="ui orange header" as='h2'><Icon name='question circle' />Match the Word to the Image!</Header></Container>}
+            
+            <Divider />
 
-            <Card.Group>
+            <Card.Group centered>
                 {!selection ? RenderCategories : <></>}
             </Card.Group>
             
