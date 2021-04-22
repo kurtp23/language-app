de8ad4d721148df65e4e6608efcf60b105163498
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-04-22 09:57:37 -0700

    clean up code with eslint

--- client/src/pages/flash-cards/FlashCard.js
+++ client/src/pages/flash-cards/FlashCard.js
@@ -26,64 +26,64 @@ const FlashCard = ({category, userState}) => {
   };
 
   useEffect(() => {
-        API.getChallengeData(category)
-        .then((data) => {
-            // console.log('data is', data.data[0].data.farmAnimals);
-             let fcList = []
-             console.log(data)
-             data.data.forEach((item) => {
-                 fcList.push({                  
-                     word: item.spa, 
-                     englishWord: item.eng
-                 }) 
-             })
-             setFlashCardList(fcList);
-             })
-    }, [])
+    API.getChallengeData(category)
+      .then((data) => {
+      // console.log('data is', data.data[0].data.farmAnimals);
+        let fcList = [];
+        console.log(data);
+        data.data.forEach((item) => {
+          fcList.push({                  
+            word: item.spa, 
+            englishWord: item.eng
+          }); 
+        });
+        setFlashCardList(fcList);
+      });
+  }, []);
     
-    useEffect(() => {
-        updateFlashCardsViewed(flashCardsViewed + 1)
-        console.log('flashcards viewed is', flashCardsViewed);
-    }, [activeFlashCard])
+  useEffect(() => {
+    updateFlashCardsViewed(flashCardsViewed + 1);
+    console.log('flashcards viewed is', flashCardsViewed);
+  }, [activeFlashCard]);
 
-    return (
-        <div style={{ width: '100%' }} className="ui raised text container segment">
-            <h2 className="ui orange header">Flash Cards: {category} </h2>
-            <div>
-                <Grid columns={1} verticalAlign='bottom'>
-                    <Grid.Row>
-                        <Grid.Column>
-                            <div style={{ height: '50vh', margins: '5px' }}>
-                                <FlashCardTemplate category={category} cardNumber={activeFlashCard} word={flashCardList[activeFlashCard -1] ? flashCardList[activeFlashCard -1].word : 'loading...'} englishWord={flashCardList[activeFlashCard -1] ? flashCardList[activeFlashCard -1].englishWord : 'loading'}/>
-                            </div>
-                        </Grid.Column>
-                    </Grid.Row>
-                    <Divider horizontal style={{ content: 'center' }}>Language Learner</Divider>
-                    <Grid.Row >
-                        <Grid.Column style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center' }} onClick={()=> console.log('I have been clicked!')}>
-                        <Pagination
-                            activePage={activeFlashCard}
-                            onPageChange={handlePaginationChange}
-                            totalPages={flashCardList.length}
-                            siblingRange={1}
-                            boundaryRange={0}
-                            ellipsisItem={null}
-                            firstItem={null}
-                            lastItem={null}
-                        />
-                        </Grid.Column>
-                        <Grid.Column style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center' }} onClick={handleExit}>
-                            <Link to="">
-                                <Button style={{ margin: '5px', backgroundColor: '#f47835', color: 'white' }} onClick={()=> console.log('I have been clicked!')}>
-                                    Exit
-                                </Button>
-                            </Link>
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div style={{ width: '100%' }} className="ui raised text container segment">
+      <h2 className="ui orange header">Flash Cards: {category} </h2>
+      <div>
+        <Grid columns={1} verticalAlign='bottom'>
+          <Grid.Row>
+            <Grid.Column>
+              <div style={{ height: '50vh', margins: '5px' }}>
+                <FlashCardTemplate category={category} cardNumber={activeFlashCard} word={flashCardList[activeFlashCard -1] ? flashCardList[activeFlashCard -1].word : 'loading...'} englishWord={flashCardList[activeFlashCard -1] ? flashCardList[activeFlashCard -1].englishWord : 'loading'}/>
+              </div>
+            </Grid.Column>
+          </Grid.Row>
+          <Divider horizontal style={{ content: 'center' }}>Language Learner</Divider>
+          <Grid.Row >
+            <Grid.Column style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center' }} onClick={()=> console.log('I have been clicked!')}>
+              <Pagination
+                activePage={activeFlashCard}
+                onPageChange={handlePaginationChange}
+                totalPages={flashCardList.length}
+                siblingRange={1}
+                boundaryRange={0}
+                ellipsisItem={null}
+                firstItem={null}
+                lastItem={null}
+              />
+            </Grid.Column>
+            <Grid.Column style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center' }} onClick={handleExit}>
+              <Link to="">
+                <Button style={{ margin: '5px', backgroundColor: '#f47835', color: 'white' }} onClick={()=> console.log('I have been clicked!')}>
+                        Exit
+                </Button>
+              </Link>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
+    </div>
+  );
+};
 
 export default FlashCard;
--- client/src/utils/audioSearch.js
+++ client/src/utils/audioSearch.js
@@ -1,33 +1,33 @@
 import Translate from './spanish';
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 
 const audioSearch = (word) => {
-    try{
-        Translate.search(word).then((data) => {
-            console.log('data is', data.data)
-            if (typeof(data.data[0]) === 'string') {
-                return ('')
-            } 
+  try{
+    Translate.search(word).then((data) => {
+      console.log('data is', data.data);
+      if (typeof(data.data[0]) === 'string') {
+        return ('');
+      } 
 
-            if (!data.data[0].hwi.prs) {
-                return ('')
-            }
-            
-            if (!data.data[0].hwi.prs[0].sound) {
-                return ('')
-            }
+      if (!data.data[0].hwi.prs) {
+        return ('');
+      }
+        
+      if (!data.data[0].hwi.prs[0].sound) {
+        return ('');
+      }
 
-            if (data.data[0].hwi.prs[0].sound.audio) {
-                console.log((`https://media.merriam-webster.com/audio/prons/es/me/mp3/${data.data[0].hwi.prs[0].sound.audio.charAt(0)}/${data.data[0].hwi.prs[0].sound.audio}.mp3`))
-                return (`https://media.merriam-webster.com/audio/prons/es/me/mp3/${data.data[0].hwi.prs[0].sound.audio.charAt(0)}/${data.data[0].hwi.prs[0].sound.audio}.mp3`)
-            }
+      if (data.data[0].hwi.prs[0].sound.audio) {
+        console.log((`https://media.merriam-webster.com/audio/prons/es/me/mp3/${data.data[0].hwi.prs[0].sound.audio.charAt(0)}/${data.data[0].hwi.prs[0].sound.audio}.mp3`));
+        return (`https://media.merriam-webster.com/audio/prons/es/me/mp3/${data.data[0].hwi.prs[0].sound.audio.charAt(0)}/${data.data[0].hwi.prs[0].sound.audio}.mp3`);
+      }
 
-            else {
-                return ('')
-            }
-        })
-    }
-    catch(err) {console.log(err)}
-}
+      else {
+        return ('');
+      }
+    });
+  }
+  catch(err) {console.log(err);}
+};
 
 export default audioSearch;
