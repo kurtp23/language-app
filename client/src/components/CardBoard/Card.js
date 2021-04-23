36c2598961b0b3771ccd3233fd201aa54b7c0e8d
rasputinforever <erik.justin.portillo@gmail.com>
2021-04-23 14:42:40 -0700

    cards flipping back

--- client/src/components/CardBoard/Card.js
+++ client/src/components/CardBoard/Card.js
@@ -1,26 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Card.css';
 
-// class Card extends React.Component {
-//   render() {
-//     let content;
-//     if(this.props.faceUp) {
-//       content = this.props.content;
-//     } else {
-//       content = '';
-//     }
-//     return (
-//       <div onClick={this.props.flip} className={`Card ${this.props.faceUp ? 'face-up': ''}`}>
-//         {content}
-//       </div>
-//     );
-//   }
-// }
+function Card({ flip, content, faceUp, index }) {
 
-function Card({ flip, content, faceUp }) {
-  
-  function handleFlip(e) {
-    flip(content)
+  function handleFlip() {
+    flip(index)
   }
 
   return (
--- client/src/pages/memory/NewBoard.js
+++ client/src/pages/memory/NewBoard.js
@@ -2,79 +2,95 @@ import React, { useEffect } from 'react';
 import Card from '../../components/CardBoard/Card';
 
 function NewBoard() {
-    const [fronts, setFronts] = React.useState([]);
-    const [deck, setDeck] = React.useState([]);
-    const [firstCard, setFirstCard] = React.useState(null)
+  const [fronts, setFronts] = React.useState([]);
+  const [deck, setDeck] = React.useState([]);
+  const [firstCard, setFirstCard] = React.useState(null)
+  const [firstCardIndex, setFirstCardIndex] = React.useState(-1)
+  
+  useEffect(() => {
+      setFronts(
+          ['🐝','💕','🌻','🧠','🚗','🚲','🚂','🧸','🍦','👾','🚀']
+      )
+  }, [])
 
-    useEffect(() => {
-        setFronts(
-            ['🐝','💕','🌻','🧠','🚗','🚲','🚂','🧸','🍦','👾','🚀']
-        )
-    }, [])
+  useEffect(() => {
+      const newDeck = [...fronts, ...fronts]
+      .sort(() => Math.random() - 0.5)
+      .map((card, index) => {
+          return {
+          content: card,
+          faceUp: false,
+          index: index,
+          };
+      });
+      setDeck(newDeck)
+      setFirstCard(null)
+  }, [fronts])
 
-    useEffect(() => {
-        const newDeck = [...fronts]
-        .sort(() => Math.random() - 0.5)
-        .map(card => {
-            return {
-            content: card,
-            faceUp: false,
-            };
-        });
-        setDeck(newDeck)
-        setFirstCard(null)
-    }, [fronts])
+  function flipCardTo(cardIdx, faceUp) {
+    console.log("got this index", cardIdx, 'and will be flipped faceUP? ', faceUp)
 
-    function flipCardTo(cardIdx, faceUp) {
-        setDeck(deck.map((f, i) => {
-                if(i === cardIdx) {
-                    return {
-                    content: f.content,
-                    faceUp: !f.faceUp,
-                    };
-                } else {
-                    return f;
-                }
-            })
-        )
-    }
+      setDeck(deck.map((f, i) => {
+              if(i === cardIdx) {
+                console.log("Match! Flip over this card, ", deck[cardIdx])
+                  return {
+                  content: f.content,
+                  faceUp: faceUp,
+                  };
+              } else {
+                  return f;
+              }
+          })
+      )
+  }
 
-    function flip(cardContent) {
-      
-      let cardIdx = deck.indexOf(deck.find(o => o.content === cardContent))
-
-        if(firstCard === null) {
-          setFirstCard(cardIdx);
+  function flip(cardIdx) {
+    
+      if(firstCard === null) {
+        setFirstCard(deck[cardIdx].content);
+        setFirstCardIndex(cardIdx);
+        
+      } else {
+        const firstCardContent = firstCard;
+        const secondCardContent = deck[cardIdx].content;
+        if(firstCardContent === secondCardContent) {
+          setFirstCard(null);
         } else {
-          const firstCardContent = deck[firstCard].content;
-          const secondCardContent = deck[cardIdx].content;
-          if(firstCardContent === secondCardContent) {
-            setFirstCard(null);
-          } else {
-            setTimeout(() => {
-              flipCardTo(firstCard, false);
-              flipCardTo(cardIdx, false);
-              setFirstCard(null);
-            }, 2000);
-          }
-        }
-    
-        flipCardTo(cardIdx, !deck[cardIdx].faceUp);
-      }
-    
+          
+          setTimeout(() => {
+          flipCardTo(cardIdx, false);
+          }, 2000);
 
-    return (
-        <>
-        {deck.map((f, i) => {
-            return (<div className="Board">
-              <Card
-                flip={flip}
-                content={f.content}
-                faceUp={f.faceUp} />
-            </div>);
-          })}
-        </>
-    )
+          setTimeout(() => {
+            flipCardTo(firstCardIndex, false);
+            setFirstCard(null);
+            setFirstCardIndex(null);
+          }, 2000);
+
+        }
+      }
+  
+      flipCardTo(cardIdx, !deck[cardIdx].faceUp);
+    }
+  
+    const gameCards = deck.map((card, i) => {
+      return (
+        <Card
+          key={i}
+          index={card.index}
+          flip={flip}
+          content={card.content}
+          faceUp={card.faceUp} />
+      )
+    })
+
+  return (
+      <>
+        <div className="Board">
+          {gameCards}
+        </div>)
+      </>
+  )
 }
 
 export default NewBoard;
