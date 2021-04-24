1622527c2e22683992b544bc400e828b504dca22
rasputinforever <erik.justin.portillo@gmail.com>
2021-04-24 10:54:31 -0700

    db now has memory game stat

--- client/src/components/MemoryGame/MemoryGameOver.js
+++ client/src/components/MemoryGame/MemoryGameOver.js
@@ -8,7 +8,7 @@ function MemoryGameOver({ userState }){
         const stat = {
             flashcardVal: 0,
             challengeVal: 0,
-            memoryVal: 0,
+            memoryVal: 1,
             date: new Date()
           };
           
