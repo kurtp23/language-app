f8e77e296ecc33e436c03d7c711a51c6ee43070e
kurtp23 <kurt_phillips@live.com>
2021-04-28 19:39:28 -0700

    added if to check for user in stats

--- client/src/components/Chart/Chart.js
+++ client/src/components/Chart/Chart.js
@@ -9,33 +9,36 @@ const Chart = (props) => {
   const [memoryGame, setMemoryGame] = useState(0);
 
   useEffect(() => {
-    API.getUser(props.userState.userId).then((data) => {
-      console.log("this is stats use effect", data.data);
-      let challenge = 0;
-      let flash = 0;
-      let memory = 0;
-      data.data[0].stats.forEach((stat) => {
-        if (stat.challengeVal) {
-          challenge++;
-        }
-        if (stat.flashcardVal) {
-          flash++;
-        }
-        if (stat.memoryVal) {
-          memory++;
-        }
+    if (props.userState.userId) {
+      API.getUser(props.userState.userId).then((data) => {
+        console.log("this is stats use effect", data.data);
+        let challenge = 0;
+        let flash = 0;
+        let memory = 0;
+        data.data[0].stats.forEach((stat) => {
+          if (stat.challengeVal) {
+            challenge++;
+          }
+          if (stat.flashcardVal) {
+            flash++;
+          }
+          if (stat.memoryVal) {
+            memory++;
+          }
+        });
+        setChallenge(challenge);
+        setFlashCard(flash);
+        setMemoryGame(memory);
       });
-      setChallenge(challenge);
-      setFlashCard(flash);
-      setMemoryGame(memory);
-    });
-  }, [props.useState]);
+    }
+  });
   console.log("chall/flash are", challenge, flashCard, memoryGame);
   const value = flashCard;
   const value2 = challenge;
   const chartPercents = `&chd=t%3A${value}%2C100%7C${value2}%2C100`;
   const chartColor = "chco=FF8033%7CFFB479%2C01BDFE%7CB5EBFE";
   const statsChart = `https://image-charts.com/chart?${chartColor}${chartPercents}&chl=Flashcards%7C%7CChallenges&chli=Stats&chs=700x300&cht=pd`;
+
   return (
     <>
       <div className="ui raised very padded text container segment">
