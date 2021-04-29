a278bc764ad00b13751d1fa61aa7e5e49ec6d0d1
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-04-29 12:44:09 -0700

    ESLint

--- client/src/components/nav/MobileNav.js
+++ client/src/components/nav/MobileNav.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Button, Icon } from "semantic-ui-react";
-import { Dropdown, Menu, Grid, Segment } from "semantic-ui-react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Icon } from 'semantic-ui-react';
+import { Dropdown, Menu, Grid, Segment } from 'semantic-ui-react';
 
 function SignOut(props) {
   return (
@@ -14,13 +14,13 @@ function SignOut(props) {
             // Sign-out successful.
           })
           .catch((error) => {
-            console.error("An error on sign out", error);
+            console.error('An error on sign out', error);
           });
       }}
     >
       <Button.Content visible>Sign Out</Button.Content>
       <Button.Content hidden>
-        <Icon name="ban" />
+        <Icon name='ban' />
       </Button.Content>
     </Button>
   );
@@ -28,42 +28,42 @@ function SignOut(props) {
 
 const MobileNav = (props) => (
   <Grid>
-    <Grid.Row columns={1} only="mobile tablet">
+    <Grid.Row columns={1} only='mobile tablet'>
       <Grid.Column>
         {/* <Segment> */}
         <Menu>
           <Menu.Item header>
-            <img src="../../project3-icon.png" />
+            <img src='../../project3-icon.png' />
           </Menu.Item>
-          <Menu.Menu position="right">
+          <Menu.Menu position='right'>
             <Menu.Item>
-              <Dropdown item text="Pages">
+              <Dropdown item text='Pages'>
                 <Dropdown.Menu>
                   <Dropdown.Item>
-                    <Link to="/">Home</Link>
+                    <Link to='/'>Home</Link>
                   </Dropdown.Item>
                   <Dropdown.Item>
-                    <Link to="/game">Game</Link>
+                    <Link to='/game'>Game</Link>
                   </Dropdown.Item>
                   <Dropdown.Item>
-                    <Link to="/challenge">Challenges</Link>
+                    <Link to='/challenge'>Challenges</Link>
                   </Dropdown.Item>
                   <Dropdown.Item>
-                    <Link to="/flashcards">Flashcards</Link>
+                    <Link to='/flashcards'>Flashcards</Link>
                   </Dropdown.Item>
                   <Dropdown.Item>
-                    <Link to="/translate">Translate</Link>
+                    <Link to='/translate'>Translate</Link>
                   </Dropdown.Item>
                   <Dropdown.Item>
-                    <Link to="/stats">Stats</Link>
+                    <Link to='/stats'>Stats</Link>
                   </Dropdown.Item>
                   <Dropdown.Item>
-                    <Link to="/settings">Settings</Link>
+                    <Link to='/settings'>Settings</Link>
                   </Dropdown.Item>
                 </Dropdown.Menu>
               </Dropdown>
             </Menu.Item>
-            <Menu.Item>{props.auth.currentUser ? SignOut(props) : "didnt work"}</Menu.Item>
+            <Menu.Item>{props.auth.currentUser ? SignOut(props) : 'didnt work'}</Menu.Item>
           </Menu.Menu>
         </Menu>
         {/* </Segment> */}
